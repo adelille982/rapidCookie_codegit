871312d8bd09fab7d4f1975202b6075883c49eb7
Anthony DELILLE <a.delille982@gmail.com>
2023-12-10 13:41:01 +0100

    ajout favicon

--- header.php
+++ header.php
@@ -7,7 +7,8 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
   <link href="https://fonts.googleapis.com/css2?family=Ephesis&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="style.css"/>
-  <title>RAPIDCOOKIE</title>
+  <link rel="icon" type="image/png" href="./rapidCookieImages/Rapidcookielogotransparenticone.ico"/>
+  <title>RapidCookie</title>
 </head>
 
 <body>
